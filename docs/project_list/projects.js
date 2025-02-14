48d109bc8361a67a7c4c439498b1d353bb9ad63e
watch1602 <watch1602@gmail.com>
2025-02-14 13:50:54 -0500

    [2025-02-14] load content from json

---
+++ docs/project_list/projects.js
@@ -0,0 +1,16 @@
+const projects = [
+    {
+        "title": "aaaa",
+        "description": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi suscipit obcaecati cumque fugit quia ullam",
+        "person": "Mason Fox",
+        "date": "1/1/2016",
+        "img": "https://s3-us-west-2.amazonaws.com/s.cdpn.io/324479/me.jpg"
+    },
+    {
+        "title": "RGA",
+        "description": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi suscipit obcaecati cumque fugit quia ullam",
+        "person": "Mason Fox",
+        "date": "1/1/2016",
+        "img": "https://s3-us-west-2.amazonaws.com/s.cdpn.io/324479/me.jpg"
+    }
+];
--- generate.py
+++ generate.py
@@ -8,7 +8,8 @@ import os
 
 __author__ = '__L1n__w@tch'
 
-if __name__ == "__main__":
+
+def generate_skills():
     json_path = os.path.join(os.path.dirname(__file__), "skills.json")
     js_path = os.path.join(os.path.dirname(__file__), "docs", "skills.js")
 
@@ -22,3 +23,22 @@ if __name__ == "__main__":
     with open(js_path, 'w', encoding='utf-8') as js_file:
         js_file.write(js_content)
     print("JSON data has been written to script.js")
+
+
+def generate_projects():
+    json_file = os.path.join(os.path.dirname(__file__), "projects.json")
+    js_file = os.path.join(os.path.dirname(__file__), "docs", "project_list", "projects.js")
+    with open(json_file, 'r') as f:
+        projects = json.load(f)
+
+    js_content = "const projects = " + json.dumps(projects, indent=4) + ";"
+
+    with open(js_file, 'w') as f:
+        f.write(js_content)
+
+    print("JSON data has been written to projects.js")
+
+
+if __name__ == "__main__":
+    generate_skills()
+    generate_projects()
